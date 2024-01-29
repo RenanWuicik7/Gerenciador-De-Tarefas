985c1545c4f426d539223c0e79aae33791af63a4
Renan Cezar Wuicik <renan.wuicik.15@gmail.com>
2024-01-28 22:44:00 -0300

    Adicionando arquivos principais dos componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import "./App.css";
+import Tasks from "./components/Tasks"
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+import { BrowserRouter as BRowter, Route, Routes } from 'react-router-dom';
+import TaskDetails from './components/Task-Details';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const handleTaskClick = (taskId) => {
+    const newTaks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+      return task;
+    });
+
+    setTasks(newTaks);
+  };
+
+  const handleTaskDelete = (taskId) => {
+    const newTaks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTaks);
+  };
+
+  const handleTaskAdition = (taskTitle) => {
+    const newTaks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      }
+    ];
+
+    setTasks(newTaks);
+  };
+
+  const exibirTarefas = () => tasks.length === 0;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BRowter>
+      <div className='container'>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <Header text="Minhas Tarefas:" />
+                <AddTask adicionarNova={handleTaskAdition} />
+                {exibirTarefas() ? <div className='aviso-inicial'>Sem tarefas por aqui!</div> :
+                  <div className='container-tasks'>
+                    <Tasks
+                      tasks={tasks}
+                      ativarTarefa={handleTaskClick}
+                      handleTaskDelete={handleTaskDelete}
+                    />
+                  </div>}
+              </>
+            }
+          />
+          <Route
+            path="/:taskTitle"
+            exact
+            element={
+              <>
+                <Header text="Descrição:" />
+                <TaskDetails />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </BRowter>
   );
-}
+};
 
 export default App;
